f51b0a0863f0f8a4bb648827e8f49c8e5599797d
SamarthK-04 <samarth.kakkar.in@shipway.com>
2025-06-20 15:17:56 +0530

    Restarting Project

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import pollsReducer from './pollsSlice';
-
-export const store = configureStore({
-  reducer: {
-    polls: pollsReducer,
-  },
-});
